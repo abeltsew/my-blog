7324535bf679e8e3f31b38f998f11fa318425f44
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-09 22:23:13 +0300

    Fix Author relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :comment
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
