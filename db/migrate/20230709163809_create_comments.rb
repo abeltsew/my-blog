9c22084eb5cbd9654887c30a6525d80df16a686a
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-09 19:54:49 +0300

    Fix references

--- db/migrate/20230709163809_create_comments.rb
+++ db/migrate/20230709163809_create_comments.rb
@@ -2,7 +2,6 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.integer :author_id
-      t.integer :post_id
       t.text :text
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
