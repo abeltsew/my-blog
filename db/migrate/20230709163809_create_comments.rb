6dcf7d4e0df232271668caa8e6d98b23fa47a017
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-09 19:59:14 +0300

    Change user_id to author_id in Migration

--- db/migrate/20230709163809_create_comments.rb
+++ db/migrate/20230709163809_create_comments.rb
@@ -1,9 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :author_id
       t.text :text
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: 'users' }
       t.references :post, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -15,24 +15,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_163809) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "author_id"
     t.text "text"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "author_id"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -56,8 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_163809) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
