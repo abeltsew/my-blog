556445b05ef2a900a7cfb12bc423742ac91d41e0
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-10 09:04:22 +0300

    create helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id
   has_many :posts, through: :likes, foreign_key: :author_id
   has_many :posts, through: :comments, foreign_key: :author_id
+
+  scope :three_recent_posts, ->(user) { user.posts.order('created_at desc').limit(3) }
 end
