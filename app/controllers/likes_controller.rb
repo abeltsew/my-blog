80218caa3082ff15e4793aa9790a16ce095ddf54
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-19 14:28:53 +0300

    fix linter

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,10 +5,9 @@ class LikesController < ApplicationController
     @like.post_id = params[:post_id]
 
     if @like.save
-        redirect_to(request.referrer || root_path)
-      else
-        render :new
-      end
-        
+      redirect_to(request.referrer || root_path)
+    else
+      render :new
+    end
   end
 end
