64fa207926d70482fa577a7508ccaad2c12b4697
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-20 15:07:41 +0300

    Fix Test to include data

--- spec/requests/post_request_spec.rb
+++ spec/requests/post_request_spec.rb
@@ -1,19 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :request do
+  before do
+    @user = User.create(name: 'Abel Tsegaye', photo: 'link to photo', bio: 'Make a diffrence', posts_counter: 0)
+    @post = Post.create(
+      title: 'Ai take over the world',
+      text: 'Ai take over the world by taking over the world',
+      author_id: @user.id,
+      comment_counter: 1,
+      likes_counter: 0
+    )
+  end
   describe '#post' do
     it 'should render the post index' do
-      get '/users/1/posts'
+      get "/users/#{@user.id}/posts"
       expect(response.status).to eq(200)
       expect(response).to render_template('posts/index')
       expect(response.body).to include 'Pagination'
     end
 
     it 'should render the post show' do
-      get '/users/1/posts/1'
+      get "/users/#{@user.id}/posts/#{@post.id}"
       expect(response.status).to eq(200)
       expect(response).to render_template('posts/show')
-      expect(response.body).to include 'Post #1 by Username'
+      expect(response.body).to include '<p>Ai take over the world by Abel Tsegaye</P>'
     end
   end
 end
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :request do
+  before do
+    @user = User.create(name: 'Abel Tsegaye', photo: 'link to photo', bio: 'Make a diffrence', posts_counter: 0)
+  end
   describe ' user#index' do
     it 'should render the users index' do
       get users_path
@@ -10,7 +13,7 @@ RSpec.describe User, type: :request do
     end
 
     it 'should render the users show' do
-      get '/users/:id'
+      get user_path(@user.id)
       expect(response.status).to eq(200)
       expect(response).to render_template('users/show')
       expect(response.body).to include 'Number of posts:'
