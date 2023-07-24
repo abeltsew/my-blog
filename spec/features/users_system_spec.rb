39c99a248342755a6e4dc83a7dd3716d0f21dcc7
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-24 18:43:54 +0300

    Add more system tests

--- spec/features/users_system_spec.rb
+++ spec/features/users_system_spec.rb
@@ -30,19 +30,50 @@ RSpec.describe 'User', type: :feature do
 
   describe 'user#show' do
     before(:each) do
-      visit user_path(@abel.id)
+      @abelt = User.create(name: 'Abel', photo: 'https://abellinktophoto.jpg', bio: 'Make a diffrence',
+                           posts_counter: 0)
+      @post1 = Post.create(title: 'post 1 title',
+                           text: 'post content 1',
+                           comment_counter: 0,
+                           likes_counter: 0,
+                           author: @abelt)
+      @post2 = Post.create(title: 'post 2 title',
+                           text: 'post content 2',
+                           comment_counter: 0,
+                           likes_counter: 0,
+                           author: @abelt)
+      @post3 = Post.create(title: 'post 3 title',
+                           text: 'post content 3',
+                           comment_counter: 0,
+                           likes_counter: 0,
+                           author: @abelt)
+      @post4 = Post.create(title: 'post 4 title',
+                           text: 'post content 4',
+                           comment_counter: 0,
+                           likes_counter: 0,
+                           author: @abelt)
+
+      visit user_path(@abelt.id)
     end
     it 'should show the profile picture for each user.' do
       expect(page).to have_xpath("//img[contains(@src,'https://abellinktophoto.jpg')]")
     end
     it 'should show the username of all other users.' do
-      expect(page).to have_content(@abel.name)
+      expect(page).to have_content(@abelt.name)
       expect(page).to_not have_content(@sam.name)
     end
-    it 'I can see the number of posts the user has written.'
-    it 'I can see the user bio.'
-    it 'I can see the user first 3 posts.'
-    it 'I can see a button that lets me view all of a users posts.'
+    it 'should show number of posts each user has written.' do
+      expect(page).to have_content 'Number of posts: 4'
+    end
+    it 'I can see the user bio.' do
+      expect(page).to have_content 'Make a diffrence'
+    end
+    it 'I can see the user first 3 posts.' do
+      expect(page).to_not have_content 'post content 4'
+    end
+    it 'I can see a button that lets me view all of a users posts.' do
+      expect(page).to have_content 'See all Posts'
+    end
     it 'When I click a users post, it redirects me to that posts show page.'
     it 'When I click to see all posts, it redirects me to the users posts index page.'
   end
