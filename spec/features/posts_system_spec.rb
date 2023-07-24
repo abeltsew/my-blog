17dae60f8c14065e01db0029a78d6681c2336fc3
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-24 18:08:42 +0000

    Add remaining tests for User post index page

--- spec/features/posts_system_spec.rb
+++ spec/features/posts_system_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe 'User', type: :feature do
       comment_counter: 0,
       likes_counter: 0,
       author: @abel)
+    @comment1 = Comment.create(text: 'First comment from Abel', author_id: @abel.id, post_id: @post1.id)
   end
 
   describe 'post index' do
@@ -22,14 +23,33 @@ RSpec.describe 'User', type: :feature do
     it 'I can see the user\'s username.' do
       expect(page).to have_content(@abel.name)
     end
-    it 'I can see the number of posts the user has written.'
-    it 'I can see a posts title.'
-    it 'I can see some of the posts body.'
-    it 'I can see the first comments on a post.'
-    it 'I can see how many comments a post has.'
-    it 'I can see how many likes a post has.'
-    it 'I can see a section for pagination if there are more posts than fit on the view.'
-    it 'When I click on a post, it redirects me to that posts show page.'
+    it 'I can see the number of posts the user has written.' do
+      expect(page).to have_content 'Number of posts: 1'
+    end
+    it 'I can see a post\'s title.' do
+      expect(page).to have_content 'post 1 title'
+    end
+    it 'I can see some of the post\'s body.' do
+      expect(page).to have_content 'post content 1'
+    end
+    it 'I can see the first comments on a post.' do
+      expect(page).to have_content 'First comment from Abel'
+    end
+    it 'I can see how many comments a post has.' do
+      expect(page).to have_content 'Comments: 1'
+      expect(page).to_not have_content 'Comments: 0'
+    end
+    it 'I can see how many likes a post has.' do
+      expect(page).to_not have_content 'Likes: 1'
+      expect(page).to have_content 'Likes: 0'
+    end
+    it 'I can see a section for pagination if there are more posts than fit on the view.' do
+      expect(page).to have_content 'Pagination'
+    end
+    it 'When I click on a post, it redirects me to that posts show page.' do
+      click_link 'post 1 title'
+      expect(page).to have_content 'post 1 title by Abel Tsegaye'
+    end
   end
 
   describe 'post show' do
