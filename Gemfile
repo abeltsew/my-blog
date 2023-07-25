a1c6f1c7d502f602c2cf3ceddbc90978af1c7709
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-25 13:49:35 +0300

    Added defaults and email service

--- Gemfile
+++ Gemfile
@@ -73,4 +73,4 @@ group :test do
   gem 'webdrivers'
 end
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_validation :set_default
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -13,4 +14,9 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
+  def set_default
+    self.name = email.split('@')[0]
+    self.posts_counter = 0
+  end
 end
