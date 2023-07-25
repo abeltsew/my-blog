59c9c4fc0047f033db96626caa63a247942235dc
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-25 16:52:42 +0300

    Add confirmable feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
   def index
     @posts = Post.where(author_id: params[:user_id])
     @user = User.find(params[:user_id])
