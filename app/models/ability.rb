b4e115dffd530a93339f0801e8d1b767375ed4df
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-26 13:04:09 +0300

    Add Autorization to delete post

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,17 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    can :read, Post, public: true
+
+    # additional permissions for logged in users (they can read their own posts)
+    return unless user.present?
+
+    can(:read, Post, user:)
+    can :destroy, Post.where(author_id: user.id)
+
+    return unless user.role == 'admin' # additional permissions for administrators
+
+    can :destroy, Post
+  end
 end
